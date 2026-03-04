b3e2c330e2e236c81c3bf35436e172f65508857d
lavolta <louis.crauffon@gmail.com>
2026-03-04 18:16:05 +0100

    feat: fetch filter airtable table

---
+++ client/src/components/Filter/FilterItem/FilterItem.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import {cn} from '../../../lib/utils'
+import './FilterItem.scss'
+
+export const FilterItem = () => {
+  return (
+    <div className="filter-item">
+        coucou
+    </div>
+  )
+};
--- client/src/hooks/useAirtable.ts
+++ client/src/hooks/useAirtable.ts
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react';
 import { createAirtableService } from '../services/airtableService';
-import { AIRTABLE_CONFIG, APP_CONFIG } from '../constants/config';
-import type { AirtableRecord } from '../types';
+import { AIRTABLE_CONFIG, APP_CONFIG, AIRTABLE_FILTER_BASE_NAME } from '../constants/config';
+import type { AirtableRecord, AiretableBaseName } from '../types';
+
+
+type AirtableFilter = {
+  key: string;              // ex: "countries"
+  tableName: AiretableBaseName; // ex: "Countries"
+  label: string;            // label UI (ici = tableName)
+  available: boolean;
+  value: []
+};
 
 /**
  * Custom hook for fetching and managing Airtable records
@@ -9,16 +18,18 @@ import type { AirtableRecord } from '../types';
 export const useAirtable = () => {
   const [records, setRecords] = useState<AirtableRecord[]>([]);
   const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState()
+  const [filtersFetched, setFiltersFetched] = useState(false)
+  const [filtersLoading, setFiltersLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const airtableService = createAirtableService(AIRTABLE_CONFIG);
 
   const fetchRecords = async () => {
     try {
       setLoading(true);
       setError(null);
-
-      const airtableService = createAirtableService(AIRTABLE_CONFIG);
-      const fetchedRecords = await airtableService.fetchRecords(
-        APP_CONFIG.defaultView,
+      const fetchedRecords = await airtableService.fetchRecordsFromTable(
+        APP_CONFIG.defaultBaseName,
         APP_CONFIG.maxRecords
       );
 
@@ -33,12 +44,51 @@ export const useAirtable = () => {
     }
   };
 
+  const fetchFilters = async () => {
+    if(filtersFetched) {
+      return
+    }
+    try {
+      setFiltersLoading(true);
+
+      const entries = Object.entries(AIRTABLE_FILTER_BASE_NAME) as Array<[string, AiretableBaseName]>;
+      const results = await Promise.all(
+        entries.map(async ([key, tableName]) => {
+          try {
+            const filterRecords = await airtableService.fetchRecordsFromTable(tableName);
+            return {
+              label: tableName,
+              available: true,
+              value: filterRecords
+            } satisfies AirtableFilter;
+          } catch {
+            return { key, tableName, label: tableName, available: false, value: [] } satisfies AirtableFilter;
+          }
+        })
+      );
+
+      // Tu peux soit garder tout (avec available), soit filtrer.
+      const availableOnly = results.filter((f) => f.available);
+      setFilters(availableOnly);
+    } finally {
+      setFiltersLoading(false);
+      setFiltersFetched(true);
+    }
+  };
+
   useEffect(() => {
     fetchRecords();
+    if(!filtersFetched) {
+      fetchFilters();
+    }
   }, []);
 
   return {
     records,
+
+    filters,
+    filtersLoading,
+
     loading,
     error,
     refetch: fetchRecords,

a39a6f0efe972d2dd9b53f94f614f493aa80af37
lavolta <louis.crauffon@gmail.com>
2026-03-05 09:06:15 +0100

    feat: adding tailwind linting

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,12 +2,17 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tailwind from 'eslint-plugin-tailwindcss'
 import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'build',
+    'node_modules',
+  ]),
 
   {
     files: ['**/*.{ts,tsx,js}'],
@@ -15,6 +20,7 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       stylistic.configs.recommended,
+      ...tailwind.configs['flat/recommended'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -30,6 +36,7 @@ export default defineConfig([
       '@stylistic/object-curly-spacing': ['error', 'always'],
       '@stylistic/space-before-function-paren': ['error', 'never'],
       '@stylistic/eol-last': ['error', 'always'],
+      'tailwindcss/no-custom-classname': 'off',
     },
   },
 ])

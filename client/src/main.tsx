aa66896037e3f3e54b735901236e0f83ed7230db
Pierre <56254358+MgRl8ow3MhVr@users.noreply.github.com>
2026-03-18 15:03:49 +0100

    style: caseCards refacto

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { Colors } from './components/ui/Colors.tsx'
 import { AirtableProvider } from '@/providers'
 import { store } from './redux/store.ts'
 import { Provider } from 'react-redux'
-
-document.documentElement.style.setProperty('--primary', Colors.primary)
-document.documentElement.style.setProperty('--secondary', Colors.secondary)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

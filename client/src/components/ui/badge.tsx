b0172da96bfb67e435b9ba1e30d3a44ec773d52b
pierre <pierre@pierre.com>
2026-03-20 11:32:48 +0100

    fix Badge width

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -7,7 +7,7 @@ interface BadgeProps {
 
 export const Badge = ({ label, color = 'black', fontColor = 'white', uppercase = false }: BadgeProps) => (
   <span
-    className={`rounded-[16px] px-[0.6rem] py-[0.25rem] text-[0.72rem] font-semibold tracking-[0.4px] whitespace-nowrap${uppercase ? ' uppercase' : ''}`}
+    className={`w-fit rounded-[16px] px-[0.6rem] py-[0.25rem] text-[0.72rem] font-semibold tracking-[0.4px] whitespace-nowrap${uppercase ? ' uppercase' : ''}`}
     style={{ backgroundColor: color, color: fontColor }}
   >
     {label}

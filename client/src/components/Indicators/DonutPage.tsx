475e4efbf6b1f4a44e9d199c83e2b42969b223a8
Philanoe <example@example.com>
2026-03-24 11:55:25 +0800

    create a simple component without parameters for the first recharts tests add a simple monochrome donut

--- client/src/components/Indicators/DonutPage.tsx
+++ client/src/components/Indicators/DonutPage.tsx
@@ -1,14 +1,41 @@
 import { useAsylumApplications } from '@/hooks/useAsylumApplications'
 import type { AsylumApplicationRecord } from '@/hooks/useAsylumApplications'
+import { PieChart, Pie, Tooltip } from 'recharts'
 
 export function DonutPage() {
-  const { records, loading, error } = useAsylumApplications()
-  return <DonutPageDetails records={records} loading={loading} error={error} />
+  // const { records, loading, error } = useAsylumApplications()
+  // return <DonutPageDetails records={records} loading={loading} error={error} />
+  return <DonutPageDetailsDev />
 }
 
 function DonutPageDetails({ records, loading, error }: { records: AsylumApplicationRecord[], loading: boolean, error: string | null }) {
   console.log(records, loading, error)
   return (
     <div>Empty page</div>
+  )
+}
+
+function DonutPageDetailsDev() {
+  const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+  ]
+
+  return (
+    <PieChart
+      style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
+      responsive
+    >
+      <Pie
+        data={data}
+        dataKey="value"
+        cx="50%"
+        cy="50%"
+        outerRadius="100%"
+        innerRadius="50%"
+      />
+      <Tooltip />
+    </PieChart>
   )
 }

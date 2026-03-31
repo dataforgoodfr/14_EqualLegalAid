7164a6db2464097d2973216711d25cefed72319c
Philanoe <example@example.com>
2026-03-31 12:07:05 +0800

    creation and use of ClickableDonut component. now, all the donuts are made from the same component, with PieChartData[] as input data for display and link control

--- client/src/components/Indicators/DonutPage.tsx
+++ client/src/components/Indicators/DonutPage.tsx
@@ -12,6 +12,35 @@ interface PieChartData {
   link: string
 }
 
+function ClickableDonut({ donutData }: { donutData: PieChartData[] }) {
+  function customShapeFunction(props: PieSectorShapeProps) {
+    return <a onClick={() => { onclick(donutData[props.index].link) }}><Sector {...props} fill={donutData[props.index].color} /></a>
+  }
+
+  return (
+    <PieChart
+      style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
+      responsive
+    >
+      <Pie
+        data={donutData}
+        dataKey="value"
+        cx="50%"
+        cy="50%"
+        outerRadius="100%"
+        innerRadius="50%"
+        shape={customShapeFunction}
+      />
+      <Tooltip />
+    </PieChart>
+  )
+}
+
+function onclick(chartLink: string) {
+  store.dispatch(setChartToDisplay(chartLink))
+  console.log({ chartLink })
+}
+
 export function DonutPage() {
   const { records, loading, error } = useAsylumApplications()
   return <DonutPageDetails records={records} loading={loading} error={error} />
@@ -22,6 +51,32 @@ function DonutPageDetails({ records, loading, error }: { records: AsylumApplicat
   console.log(records, loading, error)
   const chartName = useAppSelector(state => state.charts.chartName)
   console.log({ chartName })
+
+  const generalData: PieChartData[] = [
+    { name: 'Group A', value: 400, color: 'red', link: 'chart_number_0' },
+    { name: 'Group B', value: 300, color: 'black', link: 'chart_number_1' },
+    { name: 'Group C', value: 300, color: 'yellow', link: 'chart_number_2' },
+  ]
+
+  const detailData1: PieChartData[] = [
+    { name: 'Group A', value: 400, color: 'red', link: 'chart_number_0' },
+    { name: 'Group B', value: 300, color: 'black', link: 'chart_number_1' },
+    { name: 'Group C', value: 300, color: 'yellow', link: 'chart_number_2' },
+  ]
+
+  const detailData2: PieChartData[] = [
+    { name: 'Group A', value: 400, color: 'red', link: 'chart_number_0' },
+    { name: 'Group B', value: 300, color: 'black', link: 'chart_number_1' },
+    { name: 'Group C', value: 300, color: 'yellow', link: 'chart_number_2' },
+  ]
+
+  const detailData3: PieChartData[] = [
+    { name: 'Group A', value: 400, color: 'red', link: 'chart_number_0' },
+    { name: 'Group B', value: 300, color: 'black', link: 'chart_number_1' },
+    { name: 'Group C', value: 300, color: 'yellow', link: 'chart_number_2' },
+    { name: 'Group 4', value: 40, color: 'purple', link: '' },
+    { name: 'Group 5', value: 300, color: 'cyan', link: '' },
+  ]
 
   return (
     <>
@@ -36,100 +91,10 @@ function DonutPageDetails({ records, loading, error }: { records: AsylumApplicat
             Press me to go back to the general chart
           </button>
         )}
-      {chartName === 'global' && <GeneralPie />}
-      {chartName === 'chart_number_0' && <DetailPie />}
-      {chartName === 'chart_number_1' && <p>Chart 1</p>}
-      {chartName === 'chart_number_2' && <p>Chart 2</p>}
+      {chartName === 'global' && <ClickableDonut donutData={generalData} />}
+      {chartName === 'chart_number_0' && <ClickableDonut donutData={detailData1} />}
+      {chartName === 'chart_number_1' && <ClickableDonut donutData={detailData2} />}
+      {chartName === 'chart_number_2' && <ClickableDonut donutData={detailData3} />}
     </>
   )
 }
-
-function onclick(chartLink: string) {
-  store.dispatch(setChartToDisplay(chartLink))
-  console.log({ chartLink })
-}
-
-function GeneralPie() {
-  function customShapeFunction(props: PieSectorShapeProps) {
-    return <a onClick={() => { onclick(data[props.index].link) }}><Sector {...props} fill={data[props.index].color} /></a>
-  }
-  const data: PieChartData[] = [
-    { name: 'Group A', value: 400, color: 'red', link: 'chart_number_0' },
-    { name: 'Group B', value: 300, color: 'black', link: 'chart_number_1' },
-    { name: 'Group C', value: 300, color: 'yellow', link: 'chart_number_2' },
-  ]
-
-  return (
-    <PieChart
-      style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
-      responsive
-    >
-      <Pie
-        data={data}
-        dataKey="value"
-        cx="50%"
-        cy="50%"
-        outerRadius="100%"
-        innerRadius="50%"
-        shape={customShapeFunction}
-      />
-      <Tooltip />
-    </PieChart>
-  )
-}
-
-function DetailPie() {
-  function customShapeFunction(props: PieSectorShapeProps) {
-    return <Sector {...props} fill={data[props.index].color} />
-  }
-  const data: PieChartData[] = [
-    { name: 'Group 1', value: 400, color: 'red', link: '' },
-    { name: 'Group 2', value: 300, color: 'black', link: '' },
-    { name: 'Group 3', value: 300, color: 'yellow', link: '' },
-    { name: 'Group 4', value: 40, color: 'purple', link: '' },
-    { name: 'Group 5', value: 300, color: 'cyan', link: '' },
-    { name: 'Group 6', value: 300, color: 'green', link: '' },
-  ]
-
-  return (
-    <PieChart
-      style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
-      responsive
-    >
-      <Pie
-        data={data}
-        dataKey="value"
-        cx="50%"
-        cy="50%"
-        outerRadius="100%"
-        innerRadius="50%"
-        shape={customShapeFunction}
-      />
-      <Tooltip />
-    </PieChart>
-  )
-}
-
-// function DonutPageDetailsDev() {
-//   const chartName = useAppSelector(state => state.charts.chartName)
-//   console.log({ chartName })
-
-//   return (
-//     <>
-//       {chartName != 'global'
-//         && (
-//           <button onClick={() => {
-//             const chartName: string = 'global'
-//             store.dispatch(setChartToDisplay(chartName))
-//           }}
-//           >
-//             Press me to go back to the general chart
-//           </button>
-//         )}
-//       {chartName === 'global' && <GeneralPie />}
-//       {chartName === 'chart_number_0' && <DetailPie />}
-//       {chartName === 'chart_number_1' && <p>Chart 1</p>}
-//       {chartName === 'chart_number_2' && <p>Chart 2</p>}
-//     </>
-//   )
-// }

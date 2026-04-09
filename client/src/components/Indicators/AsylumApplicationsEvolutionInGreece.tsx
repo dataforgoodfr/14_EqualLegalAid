b231dfd13049914df61873198c64c7075a8bbe41
Philanoe <example@example.com>
2026-04-09 14:09:36 +0800

    add loading and error messages use true data in the diagram

--- client/src/components/Indicators/AsylumApplicationsEvolutionInGreece.tsx
+++ client/src/components/Indicators/AsylumApplicationsEvolutionInGreece.tsx
@@ -1,6 +1,8 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import { useGreeceTotalApplications } from '@/hooks/useGreeceTotalApplications'
 import type { AsylumApplicationRecord } from '@/hooks/useAsylumApplications'
+import { Loading } from '../Loading'
+import { ErrorMessage } from '../Caselaws/ErrorMessage'
 
 export function AsylumApplicationsEvolutionInGreece() {
   const { records, loading, error } = useGreeceTotalApplications()
@@ -8,28 +10,31 @@ export function AsylumApplicationsEvolutionInGreece() {
 }
 
 function AsylumApplicationsEvolutionInGreeceDetails({ records, loading, error }: { records: AsylumApplicationRecord[], loading: boolean, error: string | null }) {
-  const data = [
-    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-  ]
+  if (loading) return <Loading />
+  if (error) return <ErrorMessage message={error} onRetry={() => window.location.reload()} />
+
+  { /* ───────── Example of data ────────── */ }
+  // first_time_applicants: 11370
+  // id: "rec9TbyZBzbF9Tmra"
+  // name_country: "Greece"
+  // percentage: 0
+  // subsequent_applicants: 0
+  // total_applicants: 13205
+  // total_country_population: 10858018
+  // year: 2015
 
   return (
     <>
       <h2 className="text-base font-semibold">Asylum Application Evolution in Greece</h2>
-      <LineChart width={500} height={300} data={data}>
+      <LineChart width={500} height={300} data={records}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="year" />
         <YAxis />
         {/* Tooltip is used to allow interactive displaying data on mouse hover */}
         <Tooltip />
         <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line type="monotone" dataKey="first_time_applicants" stroke="#8884d8" />
+        <Line type="monotone" dataKey="subsequent_applicants" stroke="#82ca9d" />
       </LineChart>
       <button
         onClick={() => {

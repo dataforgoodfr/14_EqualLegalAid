2aa185bbb453b77c96fed4f12bf3158b3dbc40f7
Babz1711 <arnaud.baboulin@gmail.com>
2026-04-10 08:24:48 +0200

    clean

--- client/src/components/Indicators/AsylumApplicationsEvolutionInGreece.tsx
+++ client/src/components/Indicators/AsylumApplicationsEvolutionInGreece.tsx
@@ -1,9 +1,5 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import { useGreeceTotalApplications } from '@/hooks/useGreeceTotalApplications'
-import type { AsylumApplicationRecord } from '@/hooks/useAsylumApplications'
 import { AsylumApplicationsEvolutionInGreeceDetails } from '@/components/Indicators/AsylumApplicationsEvolutionInGreeceDetails'
-import { Loading } from '../Loading'
-import { ErrorMessage } from '../Caselaws/ErrorMessage'
 
 export function AsylumApplicationsEvolutionInGreece() {
   const { records, loading, error } = useGreeceTotalApplications()

991cf8ea97259d8ab11be1b27d1bb58060c6e259
Philanoe <example@example.com>
2026-04-15 18:16:25 +0800

    create a test button to check the airtable data records create useAsylumSeekerByRegionOfGreece by copy and renaming of useGreeceTotalApplications

--- client/src/components/Indicators/GreeceMap.tsx
+++ client/src/components/Indicators/GreeceMap.tsx
@@ -1,7 +1,17 @@
-import { useGreeceTotalApplications } from '@/hooks/useGreeceTotalApplications'
+import { useAsylumSeekerByRegionOfGreece } from '@/hooks/useAsylumSeekerByRegionOfGreece'
 import { GreeceMapDetails } from '@/components/Indicators/GreeceMapDetails'
 
 export function GreeceMap() {
-  const { records, loading, error } = useGreeceTotalApplications()
-  return <GreeceMapDetails records={records} loading={loading} error={error} />
+  const { records, loading, error } = useAsylumSeekerByRegionOfGreece()
+  // return <GreeceMapDetails records={records} loading={loading} error={error} />
+  return (
+    <button onClick={() => {
+      console.log({ records })
+      console.log({ loading })
+      console.log({ error })
+    }}
+    >
+      AirTable Data Checking Button
+    </button>
+  )
 }

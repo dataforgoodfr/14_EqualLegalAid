72d09e79b2aa8eb77cf4fc1d9c53699a26c28bc8
philanoe <thibault.jacquemin@mailo.com>
2026-06-15 15:33:12 +0800

    make the app caselaw page work with the new router. The menu is still absent

--- client/src/catchall.tsx
+++ client/src/catchall.tsx
@@ -1,3 +1,24 @@
+import { AirtableProvider } from '@/providers'
+import { store } from './redux/store.ts'
+import { Provider } from 'react-redux'
+import { DownloadCaselawProvider } from '@/context/'
+import{CaselawPage} from '@/pages'
+import { HeaderComponent } from '@/components/Header'
+
+
 export default function Component() {
-  return <div>Hello, world!</div>;
+  return (
+    <Provider store={store}>
+        <AirtableProvider>
+            <div className="app mx-auto my-0 w-full xl:max-w-315">
+                <HeaderComponent />
+                <main className="main-content px-4 xl:px-0">
+                    <DownloadCaselawProvider>
+                        <CaselawPage/>
+                    </DownloadCaselawProvider>
+                </main>
+            </div>
+        </AirtableProvider>
+    </Provider>
+  )
 }

5b3563356349f8da94c1605309e07382d1869067
philanoe <thibault.jacquemin@mailo.com>
2026-06-18 22:10:35 +0800

    correct the name of the component AsylumSeekersCampsPage to avoid importation error

--- client/src/pages/StatisticsPage/AsylumSeekersCampsPage.tsx
+++ client/src/pages/StatisticsPage/AsylumSeekersCampsPage.tsx
@@ -2,7 +2,7 @@ import { useOutletContext } from 'react-router-dom'
 import { AsylumSeekersCamps } from '@/components/Indicators/AsylumSeekersCamps'
 import type { StatisticOutletContext } from '@/types'
 
-export const ApplicationsEvolutionGreecePage = () => {
+export const AsylumSeekersCampsPage = () => {
   const { getCustomText } = useOutletContext<StatisticOutletContext>()
   return (
     <AsylumSeekersCamps customText={getCustomText('AsylumSeekersCamps')} />

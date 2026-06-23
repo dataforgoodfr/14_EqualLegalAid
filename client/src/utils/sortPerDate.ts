40f987d04fb94d9dd51a8b489b1bb544d936355e
Babz1711 <arnaud.baboulin@gmail.com>
2026-06-23 20:13:35 +0200

    fixes

--- client/src/utils/sortPerDate.ts
+++ client/src/utils/sortPerDate.ts
@@ -1,13 +1,19 @@
 import { useMemo } from 'react'
 import { toCaselaw } from './formatters'
 
-export const sortPerDate = (records: any[], sortDesc: boolean) => {
+export const useSortPerDate = (records: any[], sortDesc: boolean) => {
   return useMemo(() => {
     const converted = records.map(toCaselaw)
     return converted.sort((a, b) => {
       const timeA = a.publishedAt.getTime()
       const timeB = b.publishedAt.getTime()
+      if (Number.isNaN(timeA) && Number.isNaN(timeB)) return 0
+      if (Number.isNaN(timeA)) return 1
+      if (Number.isNaN(timeB)) return -1
       return sortDesc ? timeB - timeA : timeA - timeB
     })
   }, [records, sortDesc])
 }
+
+/** @deprecated Use useSortPerDate instead */
+export const sortPerDate = useSortPerDate
